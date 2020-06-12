cee9f0aa8d05a0cebca79074bf41d3dc95d4b715
heshang233 <heshang2333@outlook.com>
2020-06-12 10:24:34 +0800

    三数之和

---
+++ src/main/java/cn/huangsy/ThreeSum.java
@@ -0,0 +1,48 @@
+package cn.huangsy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author huangsy
+ * @date 2020/6/12 8:45
+ */
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i==0 || nums[i]!=nums[i-1]) {
+                int l = i+1;
+                int r = nums.length - 1;
+                int sum = 0 - nums[i];
+                while (l<r) {
+                    if (nums[l] + nums[r] == sum) {
+                        result.add(Arrays.asList(nums[i], nums[l] , nums[r]));
+                        while (l<r && nums[l]==nums[l+1]) {
+                            l++;
+                        }
+                        while (l<r && nums[r]==nums[r-1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    } else if (nums[l] + nums[r] < sum) {
+                        while (l<r && nums[l]==nums[l+1]) {
+                            l++;
+                        }
+                        l++;
+                    } else {
+                        while (l<r && nums[r]==nums[r-1]) {
+                            r--;
+                        }
+                        r--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
