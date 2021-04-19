e9c5c7f43b02a1407e39327b5d636437840ba4c2
heshang233 <heshang2333@outlook.com>
2021-04-19 09:52:01 +0800

    remove-element

--- src/main/java/cn/solution/april/Solution.java
+++ src/main/java/cn/solution/april/Solution.java
@@ -258,4 +258,27 @@ public class Solution {
         return false;
     }
 
+    /**
+     * https://leetcode-cn.com/problems/remove-element/
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i<=j) {
+            if (nums[i] == val) {
+                if (nums[j] != val) {
+                    nums[i] = nums[j];
+                    i++;
+                }
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return j+1;
+    }
+
 }
